9c21e0e3d06791b0574584cda8dc5273c262e800
Marina-Banov <m.banov7@gmail.com>
2020-01-16 19:51:13 +0100

    added global styling, fonts, navigation bar and routing

--- Agencija/src/app/app.module.ts
+++ Agencija/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OfferComponent } from './offer/offer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    OfferComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
