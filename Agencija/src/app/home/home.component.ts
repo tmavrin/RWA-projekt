8aedf928e82adc17a3fda71ef27807c8a6c58e35
Marina-Banov <m.banov7@gmail.com>
2020-01-22 23:33:16 +0100

    css refactoring

--- Agencija/src/app/home/home.component.ts
+++ Agencija/src/app/home/home.component.ts
@@ -11,19 +11,19 @@ export class HomeComponent implements OnInit {
       title: 'Culture Tour',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit, class nulla integer tristique pellentesque fringilla',
-      image: 'assets/home/example_img1.png'
+      image: 'assets/images/example_img1.png'
     },
     {
       title: 'Country Tour',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit, class nulla integer tristique pellentesque fringilla',
-      image: 'assets/home/example_img2.png'
+      image: 'assets/images/example_img2.png'
     },
     {
       title: 'Nature Tour',
       description:
         'Lorem ipsum dolor sit amet consectetur adipiscing elit, class nulla integer tristique pellentesque fringilla',
-      image: 'assets/home/example_img3.png'
+      image: 'assets/images/example_img3.png'
     }
   ];
 
