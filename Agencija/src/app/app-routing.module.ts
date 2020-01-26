62c13bb5bbf221ddd835d666b7117178e2fb4d2f
Marina-Banov <m.banov7@gmail.com>
2020-01-26 18:36:28 +0100

    add login

--- Agencija/src/app/app-routing.module.ts
+++ Agencija/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { OfferComponent } from './offer/offer.component';
+import { LoginComponent } from './login/login.component';
 import { TestApiComponent } from './test-api/test-api.component';
 
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'offer', component: OfferComponent },
+  { path: 'login', component: LoginComponent},
   { path: 'test-api', component: TestApiComponent}
 ];
 
---
+++ Agencija/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  showErrorMessage = false;
+
+  constructor(protected formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    if (this.loginForm.invalid) {
+      this.showErrorMessage = true;
+      return;
+    }
+  }
+
+}
